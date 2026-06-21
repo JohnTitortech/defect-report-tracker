5f1860f38c845c5a8764c6653f3e2214ecb59da8
JohnTitortech <farhan150407@gmail.com>
2026-06-21 13:50:39 +0700

    Update firebase.js

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -18,8 +18,6 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig)
 
-console.log("FIREBASE CONFIG", firebaseConfig)
-
 export const auth           = getAuth(app)
 export const db             = getFirestore(app)
 export const googleProvider = new GoogleAuthProvider()
