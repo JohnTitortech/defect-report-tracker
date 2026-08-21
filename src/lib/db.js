e79e05eb399582eb9af26b4877eb1d0dee066987
JohnTitortech <farhan150407@gmail.com>
2026-08-21 14:46:49 +0700

    Add picPenjawab field to data model

--- src/lib/db.js
+++ src/lib/db.js
@@ -18,6 +18,7 @@ export async function createReport(data) {
     lot:              data.lot              || '',
     problem:          data.problem          || '',
     pic:              data.pic              || '',
+    picPenjawab:      data.picPenjawab      || '',
     qty:              data.qty              ?? 1,
     responsible:      data.responsible      || [],
     cause:            data.cause            || '',
